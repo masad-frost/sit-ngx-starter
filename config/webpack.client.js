c55aba94588d3e808b59c8714ce47d7f2e142209
Faris Masad <farismasad@gmail.com>
2017-05-30 13:18:48 +0300

    Add plugins

--- config/webpack.client.js
+++ config/webpack.client.js
@@ -3,7 +3,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
 const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
 const AotPlugin = require('@ngtools/webpack').AotPlugin;
-const HotModuleReplacementPlugin = require('webpack').HotModuleReplacementPlugin;
 
 module.exports = function(options) {
   const config = {
@@ -32,22 +31,48 @@ module.exports = function(options) {
   };
 
   if (options.isProd) {
-
+    config.plugins = config.plugins.concat([
+      new webpack.optimize.UglifyJsPlugin({
+        beautify: false,
+        output: {
+          comments: false
+        },
+        mangle: {
+          screw_ie8: true
+        },
+        compress: {
+          screw_ie8: true,
+          warnings: false,
+          conditionals: true,
+          unused: true,
+          comparisons: true,
+          sequences: true,
+          dead_code: true,
+          evaluate: true,
+          if_return: true,
+          join_vars: true,
+          negate_iife: false
+        }
+      })
+    ]);
   } else {
-    // new HtmlWebpackHarddiskPlugin({
-    //   outputPath: path.resolve(__dirname, '..', 'dist', 'client')
-    // }),
-    // new HotModuleReplacementPlugin()
-    // devServer: {
-    //   compress: true,
-    //   contentBase: './src',
-    //   port: '3000',
-    //   hot: true,
-    //   inline: true,
-    //   historyApiFallback: true,
-    //   host: '0.0.0.0',
-    //   disableHostCheck: true
-    // }
+    config.plugins = config.plugins.concat([
+      new HtmlWebpackHarddiskPlugin({
+        outputPath: path.resolve(__dirname, '..', 'dist', 'client')
+      }),
+      new webpack.HotModuleReplacementPlugin()
+    ]);
+
+    config.devServer = {
+      compress: true,
+      contentBase: './src',
+      port: '3001',
+      hot: true,
+      inline: true,
+      historyApiFallback: true,
+      host: '0.0.0.0',
+      disableHostCheck: true
+    };
   }
   return config
 };
