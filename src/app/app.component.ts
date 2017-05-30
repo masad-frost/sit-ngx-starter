89dfe1880801d1e65e23c05288242ad56ec0fc75
Faris Masad <farismasad@gmail.com>
2017-05-30 16:54:19 +0300

    Remove lazy link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
   selector: 'sit-app-root',
   template: `
     <h1>SIT Angular2 Universal Starter</h1>
-    <a routerLink="/lazy">Lazy</a>
     <a routerLink="/account/login">Login</a>
     <a routerLink="/account/register">Register</a>
     <router-outlet></router-outlet>
