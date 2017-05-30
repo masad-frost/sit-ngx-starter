8f735747c261a1bfbdd67a7f99965b18ef33f452
Faris Masad <farismasad@gmail.com>
2017-05-30 16:50:29 +0300

    Consolidate auth services

--- src/app/+auth/services/auth.service.ts
+++ src/app/+auth/services/auth.service.ts
@@ -1,42 +1,51 @@
-import { Injectable, Injector } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { User } from '../models/User';
-import { Router } from '@angular/router';
-import { BehaviorSubject } from 'rxjs/Rx';
+import { HttpClient } from '../../helpers/http.service';
+
 @Injectable()
 export class AuthService {
-  public user: any;
-  public observableLoggedin = new BehaviorSubject(null);
-  private loggedIn = false;
-  private token = '';
-  private usersUrl = 'user/';  // URL to web API
+  public user: User;
+  public isLoggedIn = false;
 
-  constructor(private router: Router, private injector: Injector) {
-    if (localStorage.getItem('id_token') !== null &&
-      localStorage.getItem('id_token') !== 'false') {
-      this.setToken(localStorage.getItem('id_token'));
-    }
+  constructor(private http: HttpClient) {}
+
+  public register(user): Promise<User> {
+    return this.http.post('/register/', user)
+      .catch(this.handleError);
   }
 
-  public setToken(token) {
-    if (token) {
-      this.token = token;
-      this.loggedIn = true;
-      localStorage.setItem('id_token', token);
-    }
-  }
-
-  public getToken() {
-    return this.token;
-  }
-
-  public resetAuth() {
-    localStorage.removeItem('id_token');
-    this.token = '';
-    this.user = new User();
-    this.loggedIn = false;
+  public login(username, password): Promise<User> {
+    return this.http.post('/login/', {username, password})
+      .then((data) => {
+        this.user = new User(false, data.user);
+        this.isLoggedIn = true;
+        return this.user;
+      })
+      .catch(this.handleError);
   }
 
   public logout() {
-    this.resetAuth();
+    this.http.post('/logout/')
+      .then(() => this.isLoggedIn = false)
+      .catch(this.handleError);
+  }
+
+  public loadUser(forceReload: boolean): Promise<User> {
+    if (this.user && !forceReload) {
+      // cached
+      return Promise.resolve(this.user);
+    } else {
+      return this.http.post('/get-user/')
+        .then((data) => {
+          this.user = new User(false, data.user);
+          this.isLoggedIn = true;
+          return this.user;
+        })
+        .catch(this.handleError);
+    }
+  }
+
+  private handleError(error: any): Promise<any> {
+    return Promise.reject(error.message || error);
   }
 }
