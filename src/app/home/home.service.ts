920db2566eba665440e119bd7bbf596eae37a9e4
Faris Masad <farismasad@gmail.com>
2017-05-30 18:09:26 +0300

    Remove home service

--- src/app/home/home.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '../helpers/http.service';
-
-@Injectable()
-export class HomeService {
-  constructor(private http: HttpClient) {
-    //
-  }
-}
