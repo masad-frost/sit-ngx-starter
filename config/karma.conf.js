eb88b76f62f99f498bccf1cc032280e526e2aa97
Faris Masad <farismasad@gmail.com>
2017-06-11 17:53:19 +0300

    Fix karma config

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -1,88 +1,39 @@
-// Karma configuration
-
 module.exports = function(config) {
   var configuration = {
-
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
-
-    plugins: [
-      'karma-jasmine',
-      'karma-phantomjs-launcher',
-      'karma-chrome-launcher',
-      'karma-coverage',
-      'karma-mocha-reporter',
-      'karma-webpack',
-      'karma-sourcemap-loader',
-      'karma-junit-reporter'
-    ],
-
-    // list of files / patterns to load in the browser
     files: [
       {pattern: './config/spec-bundle.js', watched: false},
       {pattern: './src/assets/**/*', watched: false, included: false, served: true, nocache: false}
     ],
-
-
-    // list of files to exclude
-    exclude: [],
-
-    client: {
-      captureConsole: true
-    },
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-      preprocessors: {'./config/spec-bundle.js': ['coverage', 'webpack', 'sourcemap']},
-
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'mocha', 'junit'],
-
-    mochaReporter: {
-      divider: ''
-    },
-    // web server port
-    port: 9876,
-
-    /*
-     * By default all assets are served at http://localhost:[PORT]/base/
-     */
     proxies: {
       "/assets/": "/base/src/assets/"
     },
-
-
-    // enable / disable colors in the output (reporters and logs)
+    client: {
+      captureConsole: true
+    },
+    webpack: require('./webpack.test.js'),
+    webpackMiddleware: {
+      noInfo: true
+    },
+    coverageReporter: {
+      type: 'in-memory'
+    },
+    remapCoverageReporter: {
+      'text-summary': null,
+      json: './coverage/coverage.json',
+      html: './coverage/html'
+    },
+    preprocessors: {'./config/spec-bundle.js': ['coverage', 'webpack', 'sourcemap']},
+    reporters: ['mocha', 'coverage', 'remap-coverage'],
+    mochaReporter: {
+      divider: ''
+    },
+    port: 9876,
     colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS', 'Chrome'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
+    browsers: ['PhantomJS'],
+    singleRun: true,
     concurrency: Infinity
   };
   config.set(configuration);
